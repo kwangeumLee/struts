49000159dd79c93293c9ca71ee0b7dbae67b4d35
kwangeumLee <kelee@lsis.com>
2019-08-23 09:47:33 +0900

    struts

---
+++ src/main/java/org/superbiz/struts/Application.java
@@ -0,0 +1,8 @@
+package org.superbiz.struts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+}
